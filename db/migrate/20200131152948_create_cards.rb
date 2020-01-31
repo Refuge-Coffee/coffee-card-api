ec1b6f493f0ed356a323a844bf37d180a14c254c
hiimtaylorjones <taylorjonessoftware@gmail.com>
2020-01-31 10:51:18 -0500

    initial draft of migrations

--- db/migrate/20200131152948_create_cards.rb
+++ db/migrate/20200131152948_create_cards.rb
@@ -1,7 +1,8 @@
 class CreateCards < ActiveRecord::Migration[6.0]
   def change
     create_table :cards do |t|
-
+      t.string :full_number
+      t.integer :balance_cents
       t.timestamps
     end
   end
--- db/migrate/20200131153000_create_events.rb
+++ db/migrate/20200131153000_create_events.rb
@@ -1,7 +1,7 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-
+      t.belongs_to :card
       t.timestamps
     end
   end
