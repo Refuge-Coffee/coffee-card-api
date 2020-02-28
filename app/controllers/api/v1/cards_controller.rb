622436780c5bdc454f0003f3f73057931c62f59e
hiimtaylorjones <taylorjonessoftware@gmail.com>
2020-02-28 16:46:47 -0500

    making search work with ember frontend

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::CardsController < ApplicationController
   def search
+    params = request_body_params
+
     card = Card.find_by(full_number: params[:card_number])
     if card.present?
       render json: CardBlueprint.render(card, view: :normal)
@@ -8,5 +10,12 @@ class Api::V1::CardsController < ApplicationController
     else
       render json: {}, status: 400
     end
+  end
+
+private 
+
+  def request_body_params
+    body = request.body.read
+    JSON.parse(body).symbolize_keys
   end
 end
